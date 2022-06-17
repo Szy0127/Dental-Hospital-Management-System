7b4005bd2c209ad6897ee44c4b7589da20df40ca
Szy0127 <1799853523@qq.com>
2022-06-17 21:06:00 +0800

    cycle
    Signed-off-by: Szy0127 <1799853523@qq.com>

--- back_end/src/main/java/com/sjtu/se/hospital/entity/Department.java
+++ back_end/src/main/java/com/sjtu/se/hospital/entity/Department.java
@@ -1,6 +1,8 @@
 package com.sjtu.se.hospital.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -10,6 +12,7 @@ import java.util.List;
 @Entity
 @Table(name = "department")
 @JsonIgnoreProperties(value = {"handler","hibernateLazyInitializer","fieldHandler"})
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "id")
 public class Department {
 
     @Id
